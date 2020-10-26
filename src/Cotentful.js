25649691813fdc98458f26e40e4451d1aa2c8ba7
shady2498 <shahhassancs@gmail.com>
2020-10-26 17:10:15 +0500

    uploading on netlify

--- src/Cotentful.js
+++ src/Cotentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-	space: "g1idcnucvf3i",
-	accessToken: "xUg4IH5IUo_8ZHosehmaafrMXVZQaFLEqVrpEGCetsQ",
+	space: process.env.REACT_APP_API_SPACE,
+	accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
